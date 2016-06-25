2cb635accc53de263bf7ec171cf84f81e982c025
egan <edone@sympatico.ca>
2016-06-25 19:03:24 -0400

    Added code for simple 2 led blinker

---
+++ simple_blink/simple_blink/simple_blink.ino
@@ -0,0 +1,19 @@
+int timer = 100;
+int pins[] = {0,1};
+int pinCount = sizeof(pins) / sizeof(int);
+
+void setup() {
+  for (int thisPin = 0; thisPin < pinCount; thisPin++)  {
+    pinMode(pins[thisPin], OUTPUT);      
+  }
+}
+
+void loop() {
+  int pinIndex = random(0, pinCount);
+
+  if ((pinIndex >= 0) && (pinIndex < pinCount)) {
+        digitalWrite(pins[pinIndex], HIGH);   
+    delay(timer);                  
+        digitalWrite(pins[pinIndex], LOW);    
+  }
+}
