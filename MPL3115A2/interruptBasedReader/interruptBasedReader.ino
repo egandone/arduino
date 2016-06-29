7856da8d3af77f96213a30388ae322805b406b17
egan <edone@sympatico.ca>
2016-06-29 09:43:10 -0400

    Added on LCD to display temperature and pressure. Still polling

--- MPL3115A2/interruptBasedReader/interruptBasedReader.ino
+++ MPL3115A2/interruptBasedReader/interruptBasedReader.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_RGBLCDShield.h>
 
 // I2C ADDRESS of the MPL311A2
 #define MPL3115A2_ADDRESS                       (0x60)    // 1100000
@@ -123,9 +125,21 @@ float getPressure() {
   return baro;
 }
 
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting setup() ...");
+  lcd.begin(16, 2);
+  lcd.setBacklight(WHITE);
   
   Wire.begin();
   uint8_t whoami = read8(MPL3115A2_WHOAMI);
@@ -138,7 +152,7 @@ void setup() {
       MPL3115A2_PT_DATA_CFG_TDEFE |
       MPL3115A2_PT_DATA_CFG_PDEFE |
       MPL3115A2_PT_DATA_CFG_DREM);
-    write8(MPL3115A2_CTRL_REG2, 0x06);
+    write8(MPL3115A2_CTRL_REG2, 0x00);
   } else {
     Serial.println(" *** ERROR couldn't find MPL3115A2 ***");
   }
@@ -154,7 +168,15 @@ void loop() {
   Serial.print("Temperature is ");
   Serial.print(temperature);
   Serial.println("C");
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(temperature);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print(pressure);
+  lcd.print("kPa");
   
-  delay(5000);
+  delay(500);
 }
 
